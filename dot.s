12c050d06c7c09000ca666f87157563fed769099
Margarida <mj.magui.097@gmail.com>
2026-04-27 21:25:11 +0100

    Update dot.s

--- dot.s
+++ dot.s
@@ -1,55 +1,47 @@
+# You can change these values to test your solution.
 .data
-# You can change these arrays to test other values
-A: .word -3, 2, -1, 7, -2   # Initial array A values				 
-B: .word 6, 1, -1, 3, 2   # Initial array B values
+A:    .word 6, 1, 3, 9, 12, 4, 13, 153
+B:    .word 6, 1, 3, 9, 12, 4, 13, 153
+SIZE: .word 8
 
 .text
-
 main:
-  la a0, A          # a0 = pointer to array A
-  la a1, B          # a1 = pointer to array B
-  li a2, 5          # a2 = number of elements in each array
-
-  jal ra, dot      # Call dot function
-
-  # Result: a0 contains the dot product of the two integer arrays
-
+  la a1, A          # a1 = pointer to array A
+  la a2, B          # a2 = pointer to array B
+  lw a3, SIZE       # a3 = number of elements in each array
+  jal ra, dot       # call dot function
 exit:
-  li a7, 10              # Exit syscall code
-  ecall                  # Terminate the program
-
+  li a7, 10         # exit syscall code
+  ecall             # terminate the program
 
 # ==========================================================================
 # FUNCTION: dot
 #   This function computes the dot product of two integer arrays.
 # Arguments:
-#   a0 = pointer to first array
-#   a1 = pointer to second array
-#   a2 = array length
+#   a1 = pointer to first array
+#   a2 = pointer to second array
+#   a3 = array length
 # Returns:
-#   a0 = dot product result
-# Exceptions:
-#   - If the length of the array is less than 1,
-#     this function terminates the program with error code 50
+#   a0 = status code
+#   a1 = dot product result
 # ===========================================================================
 dot:
-	li t0, 1
-    blt a2, t0, invalid_length
-	blez a2, invalid_length
+  	li t0, 1
+	blt a3, t0, invalid_length
 
 	li t0, 0 		# accumulator
 	li t1, 0 		# index
 
 	loop_start:
-		bge t1, a2, loop_end
+		bge t1, a3, dot_end
 
 		slli t2, t1, 2
 
 		# A
-		add t3, a0, t2 			# t3 = A[i]'s address
+		add t3, a1, t2 			# t3 = A[i]'s address
         lw t3, 0(t3) 			# t3 = A[i]'s value
 		# B
-		add t4, a1, t4 			# t4 = A[i]'s address
+		add t4, a2, t2 			# t4 = A[i]'s address
         lw t4, 0(t4) 			# t4 = A[i]'s value
 
 		mul t5, t3, t4 			# multiplies the values
@@ -60,19 +52,10 @@ dot:
 
     invalid_length: 
 		li a0, 50
-		j exit_with_error
+		li a1, 0
+		jr ra
 
-loop_end:
-	mv a0, t0
-    jr ra                  # normal return
-
-
-# Exits the program with an error 
-# Arguments: 
-# a0 (int) is the error code 
-# You need to load a0 the error to a0 before to jump here
-exit_with_error:
-  li a7, 93            # Exit system call
-  ecall                # Terminate program
-
-
+dot_end:
+	li a0, 0
+	mv a1, t0
+  	jr ra               # return to the caller
