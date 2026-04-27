24d78b4dd553b695b93907a252922e375a643014
sillymamaco <cojocariirina567@gmail.com>
2026-04-27 16:08:40 +0100

    updated argmax.s

--- argmax.s
+++ argmax.s
@@ -1,21 +1,16 @@
+# You can change these values to test your solution.
 .data
-# You can change this array to test other values
-array: .word -3, 2, -1, 7, -2   # Initial array values				 
+ARRAY: .word -6 -1 6 1
+SIZE:  .word 4
 
 .text
-
 main:
-  la a0, array      # a0 = pointer to array
-  li a1, 5          # a1 = number of elements in the array
-
-  jal ra, argmax      # Call argmax function
-
-  # Result: a0 contains the index of the largest element
-
+  la a1, ARRAY        # a1 = pointer to array
+  lw a2, SIZE         # a2 = number of elements in the array
+  jal ra, argmax      # call argmax function
 exit:
-  li a7, 10              # Exit syscall code
-  ecall                  # Terminate the program
-
+  li a7, 10           # exit syscall code
+  ecall               # terminate the program
 
 # ==========================================================================
 # FUNCTION: argmax
@@ -23,27 +18,25 @@ exit:
 #   If there are multiple elements with the same maximum value, 
 #   it should return the smallest index among them.
 # Arguments:
-#   a0 = pointer to int array
-#   a1 = array length
+#   a1 = pointer to int array
+#   a2 = array length
 # Returns:
-#   a0 = index of the largest element
-# Exceptions:
-#   - If the length of the array is less than 1,
-#     this function terminates the program with error code 50
+#   a0 = status code
+#   a1 = index of the largest element
 # ===========================================================================
 argmax:
+  # TODO: Implement the argmax function here
+   blez a2, invalid_length
 
-    blez a1, invalid_length
-
-    lw t0, 0(a0) #current biggest
+    lw t0, 0(a1) #current biggest
     li t1, 0 #current max index
     li t2, 1 #current index
 
     loop_start:
-        bge t2, a1, loop_end
+        bge t2, a2, loop_end
 
         slli t4, t2, 2
-        add t4, a0, t4
+        add t4, a1, t4
         lw t3, 0(t4)
 
         ble t3, t0, skip_update
@@ -53,22 +46,17 @@ argmax:
     skip_update:
         addi t2, t2, 1
         j loop_start
-
-
+    loop_end:
+        mv a1, t1
+        li a0, 0
+        j argmax_end
     invalid_length:
         li a0, 50
-        j exit_with_error
+        j argmax_end
 
-    loop_end:
-        add a0, t1, x0
-        jr ra                  # normal return
+              
 
+argmax_end:
+    jr ra               # return to the caller
 
-# Exits the program with an error 
-# Arguments: 
-# a0 (int) is the error code 
-# You need to load a0 the error to a0 before to jump here
-exit_with_error:
-  li a7, 93            # Exit system call
-  ecall                # Terminate program
 
