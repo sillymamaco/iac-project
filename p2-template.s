129c678752564a33e40e1688bb649fed71179164
Margarida <mj.magui.097@gmail.com>
2026-05-14 15:44:00 +0100

    Add read_file function

--- p2-template.s
+++ p2-template.s
@@ -167,8 +167,23 @@ main:
 # (in)     a0: filename address (char*)
 # (in/out) a1: destination buffer
 # (in)     a2: maximum number of bytes to read
-read_file:  #MARGARIDA
-    # TODO
+read_file:
+    li a7, 1024										# Open File
+    mv s0, a0
+    mv s1, a1
+    li a1, 0
+    ecall
+
+    mv a1, s1
+    li a7, 63										# Read File
+    ecall
+
+    mv t0, a0
+    li a7, 57										# Close File
+    ecall
+
+    mv a0, t0
+    jr ra
 
 # Assumes the matrix is stored in the buffer as space-separated integers.
 # Assumes columns are separated by 1 space (' '), and rows by 1 newline ('\n').
